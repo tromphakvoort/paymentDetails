ff1829547910f9dbe05267e4168dba4caf140bc7
Tromp Hakvoort <tromphakvoort@outlook.com>
2021-03-05 13:25:26 +0100

    Added ngoninit function

--- PaymentDetailsFrontEnd/src/app/payment-details/payment-details.component.ts
+++ PaymentDetailsFrontEnd/src/app/payment-details/payment-details.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { PaymentDetailService } from '../shared/payment-detail.service';
 
 @Component({
   selector: 'app-payment-details',
   templateUrl: './payment-details.component.html',
-  styleUrls: ['./payment-details.component.scss']
+  styleUrls: ['./payment-details.component.scss'],
 })
 export class PaymentDetailsComponent implements OnInit {
-
-  constructor() { }
+  constructor(public service: PaymentDetailService) {}
 
   ngOnInit(): void {
+    this.service.refreshList();
   }
-
 }

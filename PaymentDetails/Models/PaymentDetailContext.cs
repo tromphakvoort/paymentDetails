59fcccf2f819898571138ec4f6d2da563f7276aa
Tromp Hakvoort <tromphakvoort@outlook.com>
2021-03-05 11:25:09 +0100

    AddedMigrations

--- PaymentDetails/Models/PaymentDetailContext.cs
+++ PaymentDetails/Models/PaymentDetailContext.cs
@@ -5,7 +5,7 @@ namespace PaymentDetails.Models
 {
     public class PaymentDetailContext : DbContext
     {
-        public PymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
         { }
 
         public DbSet<PaymentDetail> PaymentDetails { get; set; }

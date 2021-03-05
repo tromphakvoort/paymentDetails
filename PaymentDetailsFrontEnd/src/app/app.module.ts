8338ddb923d57dd7a323e3fc7743bf779e0255db
Tromp Hakvoort <tromphakvoort@outlook.com>
2021-03-05 11:51:03 +0100

    Added Components

--- PaymentDetailsFrontEnd/src/app/app.module.ts
+++ PaymentDetailsFrontEnd/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailsFormComponent } from './payment-details-form/payment-details-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PaymentDetailsComponent, PaymentDetailsFormComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ PaymentDetailsFrontEnd/src/app/payment-details-form/payment-details-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-details-form',
+  templateUrl: './payment-details-form.component.html',
+  styleUrls: ['./payment-details-form.component.scss']
+})
+export class PaymentDetailsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

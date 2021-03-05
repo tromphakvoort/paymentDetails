a88195d9b23c2822977e55af0cce1c95a4d12c41
Tromp Hakvoort <tromphakvoort@outlook.com>
2021-03-05 12:42:05 +0100

    Updated app.module.ts

--- PaymentDetailsFrontEnd/src/app/app.module.ts
+++ PaymentDetailsFrontEnd/src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailsFormComponent } from './payment-details-form/payment-details-form.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [AppComponent, PaymentDetailsComponent, PaymentDetailsFormComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailsFormComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
